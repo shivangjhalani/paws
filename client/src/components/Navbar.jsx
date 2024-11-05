fc9fa38f79618e92cb86dc807f7b1ef89dc994bc
shivangjhalani <shivang2004jhalani@gmail.com>
2024-11-05 14:33:57 +0530

    logo updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,11 +30,7 @@ const Navbar = () => {
         <Link to="/">
           <div className="flex items-center gap-2">
             <div className="h-14 w-14">
-              <img
-                src="/paw.png"
-                alt="Pawfect Logo"
-                className="h-full w-full p-2 rounded-full"
-              />
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-paw-print"><circle cx="11" cy="4" r="2" /><circle cx="18" cy="8" r="2" /><circle cx="20" cy="16" r="2" /><path d="M9 10a5 5 0 0 1 5 5v3.5a3.5 3.5 0 0 1-6.84 1.045Q6.52 17.48 4.46 16.84A3.5 3.5 0 0 1 5.5 10Z" /></svg>
             </div>
             <span className="font-bold text-3xl">pawfect</span>
           </div>
